9b3add89a814e84055b5fcae134e6907207afe8e
unknown <maufdezessete@gmail.com>
2022-10-12 15:51:39 -0300

    adiciona medida de segurança contra sql injection

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/service/user.service';
@@ -19,22 +20,38 @@ export class LoginComponent implements OnInit {
   userModel = new User()
   mensagem = ""
 
+
   receberDados() {
 
-    console.log(this.userModel);
+    if (this.userModel.email?.includes('SELECT')||this.userModel.email?.includes('DROP')|| 
+    this.userModel.password?.includes('SELECT')||this.userModel.password?.includes('DROP')) {
+
+      alert('AS PALAVRAS "SELECT" E "DROP" NÃO SÃO PERMITIDAS, DIGITE NOVAMENTE!')
+      
+     } else {
+
+      console.log(this.userModel);
+
+      this.userService.validarLogin(this.userModel).subscribe({
+        next: (sucesso) => {
+          console.log('deu certo');
+          console.log(sucesso);
+          this.mensagem = 'Logado com sucesso'
+        },
+        error: (erro) => {
+          console.log('deu erro');
+          console.log(erro);
+          this.mensagem = 'Senha ou e-mail inválido'
+  
+        }
+      })
+     }
+
     
-    this.userService.validarLogin(this.userModel).subscribe({
-      next: (sucesso) => {
-        console.log('deu certo');
-        console.log(sucesso);
-        this.mensagem = 'Logado com sucesso'
-      },
-      error: (erro) => {
-        console.log('deu erro');
-        console.log(erro);
-        this.mensagem = 'Senha ou e-mail inválido'
-      }
-    })
+    
+   
+
+
 
   }
 
